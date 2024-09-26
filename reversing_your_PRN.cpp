3aca8fbaf4b2c43261084f197f53d7b0a0053349
Arav Upadhyay <89582303+FlaShOPArav@users.noreply.github.com>
2024-09-26 11:36:34 +0530

    Create reversing_your_PRN.cpp

---
+++ reversing_your_PRN.cpp
@@ -0,0 +1,27 @@
+//Arav Upadhyay
+//23070123026
+//Aim - To reverse your PRN using while loops.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+     int PRN,reverse;
+    cout<<"Enter your PRN:"<<endl;
+    cin>>PRN;
+    while(PRN!=0){
+        reverse=PRN%10;
+        PRN=PRN/10;
+        cout<<reverse;
+    }
+        
+    return 0;
+}
+
+
+/*
+Output
+Enter your PRN:
+23070
+07032
+*/
