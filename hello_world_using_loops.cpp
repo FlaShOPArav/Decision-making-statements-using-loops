405d5201eb1ed076032f16ddb35775bf49488ee0
Arav Upadhyay <89582303+FlaShOPArav@users.noreply.github.com>
2024-09-26 11:33:53 +0530

    Create hello_world_using_loops.cpp

---
+++ hello_world_using_loops.cpp
@@ -0,0 +1,24 @@
+//Arav Upadhyay
+//23070123026
+//Aim - print hello world using for loop for n number of times.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "hello world" << endl;
+    }
+    return 0;
+}
+
+/* 
+OUTPUT
+hello world
+hello world
+hello world
+hello world
+hello world
+*/
